ccad1a4e7b566c4882a1b90ab17cb3b19c4735e5
Anna Shevelyova <99125817+shvlvnn@users.noreply.github.com>
2022-03-05 23:34:54 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -30,15 +30,15 @@ double expn(double x, uint16_t count) {
 double sinn(double x, uint16_t count) {
     double s = 0;
     for (uint16_t i = 1; i <= count; ++i) {
-            s += pown(-1, i)*calcItem(x, 2*i-1);
+            s += pown(-1, i-1)*calcItem(x, 2*i-1);
     }
     return s;
 }
 
 double cosn(double x, uint16_t count) {
     double c = 0;
-    for (uint16_t i = 0; i <= count; ++i) {
-        c += pown(-1, i)*calcItem(x, 2*i-2);
+    for (uint16_t i = 1; i <= count; ++i) {
+        c += pown(-1, i-1)*calcItem(x, 2*i-2);
     }
     return c;
 }
